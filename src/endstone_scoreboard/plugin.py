f78da69995af70b5e2bcd3e7efaa568a91229843
Lumora Studios <asepudinbalap@gmail.com>
2026-08-31 21:07:03 +0700

    Remove author comment from plugin.py
    Removed author comment from the plugin file.

--- src/endstone_scoreboard/plugin.py
+++ src/endstone_scoreboard/plugin.py
@@ -2,8 +2,6 @@
 #
 # Structured so other plugins can plug their own placeholders in instead of
 # forking this code - see api.py for the part that matters to them.
-#
-# Author: Appolo
 
 from pathlib import Path
 from typing import Dict
